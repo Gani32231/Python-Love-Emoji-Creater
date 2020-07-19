a92b91915eed3556b222b167800df596b68a25d0
Gani32231 <49157943+Gani32231@users.noreply.github.com>
2020-07-19 13:04:11 +0530

    Add files via upload

---
+++ lovesp.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  1 23:38:02 2020
+
+@author: Dhondi Sai Ganesh
+"""
+
+
+import turtle
+t=turtle.Turtle()
+t.color("red")
+t.begin_fill()
+t.fillcolor("red")
+t.left(140)
+t.forward(180)
+t.circle(-90,200)
+t.setheading(60)
+t.circle(-90,200)
+t.forward(180)
+t.end_fill()
